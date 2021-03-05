a1366451aa236868005e45eed638e152f9911f64
Pablo Alvarado <pablo.andre_alvarado@hotmail.com>
2021-03-04 21:07:28 -0600

    Implementación de parentesis en la validación de textos

--- validarTexto.js
+++ validarTexto.js
@@ -1,17 +1,18 @@
 // Colocar en la siguiente variable el texto de entrada que se quiere evaluar
-const entryText = '{Hola, [uno, dos, tres], hola de nuevo, [Adios]}';
+const entryText = '{Hola, [uno, dos, tres], hola de (nuevo, [Adios]}';
 const letterArray = entryText.split('');
 var res = true;
 
 function validarTexto(num) {
   var curlyBrackets = false;
   var squareBrackets = false;
+  var parentesis = false;
   for (var i = num; i < letterArray.length; i++) {
     // Si se abre una llave o un corchete, se guarda el estado y se introduce a un nuevo nivel.
-    if (letterArray[i] == '{' || letterArray[i] == '[') {
-      letterArray[i] == '{' ? (curlyBrackets = true) : (squareBrackets = true);
+    if (letterArray[i] == '{' || letterArray[i] == '[' || letterArray[i] == '(') {
+      letterArray[i] == '{' ? (curlyBrackets = true) : (letterArray[i] == '[' ? squareBrackets = true : parentesis = true);
       i = validarTexto(++i);
-    } else if (letterArray[i] == '}' || letterArray[i] == ']') {
+    } else if (letterArray[i] == '}' || letterArray[i] == ']' || letterArray[i] == ')') {
       // Si se encontró un cierre de llave o corchete se sale del nivel actual para darle cierre en el nivel superior (donde fue abierto)
       return i;
     } else {
@@ -22,13 +23,13 @@ function validarTexto(num) {
     // Se hace un try porque si fue una cadena inválida en el pasado, se retorna la logitud de la cadena, lo cuál está fuera del indice del arreglo de caracteres
     try {
       // Intenta darle cierre a una llave o un corchete.
-      if ((letterArray[i] == '}' && !curlyBrackets) || (letterArray[i] == ']' && !squareBrackets)) {
+      if ((letterArray[i] == '}' && !curlyBrackets) || (letterArray[i] == ']' && !squareBrackets) || (letterArray[i] == ')' && !parentesis)) {
         console.log('La cadena introducida es inválida.');
         res = false;
         return letterArray.length;
       } else {
         // Si se cierra correctamente una llave o un corchete, se restablecen las variables para seguir analizando el mismo nivel
-        letterArray[i] == '{' ? (curlyBrackets = false) : (squareBrackets = false);
+        letterArray[i] == '{' ? (curlyBrackets = false) : (letterArray[i] == '[' ? squareBrackets = false : parentesis = false);
       }
     } catch(e) {
       break;
